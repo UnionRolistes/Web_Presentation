384f6a326fbc51b7ca1320b7a0ec2ceeb6c306ac
MoonLyss <shaitan.lyssm@gmail.com>
2021-06-30 21:36:01 +0200

    Add auto removal of unfilled fields

--- Form/cgi/pres/create_presentation.py
+++ Form/cgi/pres/create_presentation.py
@@ -1,5 +1,6 @@
 #!python
 import re
+import sys
 
 from discord import Webhook, RequestsWebhookAdapter
 import discord
@@ -12,40 +13,54 @@ cgitb.enable(display=0, logdir="d:/wamp64/logs/cgi")
 # Getting form data
 # form = cgi.FieldStorage()
 
-def get_payload(_form: cgi.FieldStorage) -> str:
+def get_payload(form: cgi.FieldStorage) -> str:
     """ Process form data to create webhook payload. """
     res = ""
+    checks = []
+    if form.getvalue('news'):
+        checks.append("**News: ** ✅")
+    if form.getvalue('gn'):
+        checks.append("**GN: ** ☑")
+
+    kwargs = {
+        'pseudo': f"<@{form['user_id'].value}> [{form['pseudo'].value}]",
+        'home': f"{form.getvalue('region')}{' - ' + form.getvalue('ville') if form.getvalue('ville') else ''}",
+        'age': form['age'].value if form.getvalue('age') else form['trancheAge'].value,
+        'experience': form.getvalue('experience'),
+        'origin':  form.getvalue('connaissance'),
+        'hobby': form.getvalue('hobby'),
+        'mj_pj': form.getvalue('typeJoueur'),
+        'jdr': form.getvalue('JDR'),
+        'i_like': form.getvalue('like'),
+        'i_dislike': form.getvalue('dislike'),
+        'availability': form.getvalue('dispos'),
+        'jobs': form.getvalue('job'),
+        'other': form.getvalue('autre'),
+        'free_expression': form.getvalue('expression'),
+        'checks': "  **|**  ".join(checks)
+
+    }
+
     with open("pres_template.txt", 'r', encoding='utf-8') as f:
         for line in f:
+            is_empty = True
             # checks line is not a comment
             if not re.match(" *#", line):
-                res += line.format(
-                    pseudo=f"<@{_form['user_id'].value}> [{_form['pseudo'].value}]",
-                    region=_form['region'].value,
-                    city=_form['ville'].value,
-                    age=_form['age'].value if _form.getvalue('age') else _form['trancheAge'].value,
-                    experience=_form['experience'].value,
-                    origin=_form['connaissance'].value,
-                    hobby=_form['hobby'].value,
-                    mj_pj=_form['typeJoueur'].value,
-                    jdr=_form['JDR'].value,
-                    i_like=_form['like'].value,
-                    i_dislike=_form['dislike'].value,
-                    availability=_form['dispos'].value,
-                    jobs=_form['job'].value,
-                    other=_form['autre'].value,
-                    free_expression=_form['expression'].value,
-                    news='Oui' if _form.getvalue('news') else 'Non',
-                    gn='Oui' if _form.getvalue('gn') else 'Non'
-                )
+                for match in re.finditer("{([A-Za-z0-9_]*)}", line):
+                    key = match.group(1)
+                    if key in kwargs and kwargs[key]:
+                        is_empty = False
+                        break
+
+                if not is_empty:
+                    res += line.format(**kwargs)
     return res
 
 
 def get_webhook_url(_form: cgi.FieldStorage) -> str:
     return _form['webhook_url'].value
 
-
-if __name__ == '__main__':
+def main():
     try:
         form = cgi.FieldStorage()
         webhook = Webhook.from_url(get_webhook_url(form), adapter=RequestsWebhookAdapter())
@@ -53,9 +68,14 @@ if __name__ == '__main__':
     except Exception as e:
         print("Content-Type: text/html")    # HTML is following
         print()
-        print(cgitb.html(e))
+        raise e
+
+        # print(cgitb.html(sys.exc_info()))
     else:
         # Redirects to main page
         print("Status: 303 See other")
         print("Location: http://93.11.4.50.nip.io")
         print()
+
+if __name__ == '__main__':
+    main()
