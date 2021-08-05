0e92c68bafa69fc0a241ffb90fe320bbfd3fd212
Youssef <agoussalyo@eisti.eu>
2021-08-05 12:02:20 +0200

    Changed footer to CC BY NC SA

--- src/www/php/footer.php
+++ src/www/php/footer.php
@@ -1,9 +1,8 @@
 <footer>           
     <div class="titleCenter">
-        <h5><a rel="license" href="http://creativecommons.org/licenses/by/2.0/fr/">
-            <img alt="Licence Creative Commons" style="border-width:0" src="img/ccby.png"/>
-        </a> 
-        &copy; 2021 Page créée par <a href="http://unionrolistes.fr/">l'Union Des Rôlistes</a>. Association et marque deposée. Version BETA</h5>
-        Ce(tte) œuvre est mise à disposition selon les termes de la <a rel="license" href="http://creativecommons.org/licenses/by/2.0/fr/">Licence Creative Commons Attribution 2.0 France</a>
+        <h5>&copy; 2021 Page créée par <a href="http://unionrolistes.fr/">l'Union Des Rôlistes</a>. Association et marque deposée. Version BETA</h5>
+        Creative Commons: Attribution - Pas d’Utilisation Commerciale - Partage dans les Mêmes Conditions 4.0 International
+(nous contacter pour toute dérogation)
+        <p xmlns:cc="http://creativecommons.org/ns#" xmlns:dct="http://purl.org/dc/terms/"><a property="dct:title" rel="cc:attributionURL" href="https://github.com/UnionRolistes">UR_Bot</a> by <a rel="cc:attributionURL dct:creator" property="cc:attributionName" href="http://contact.unionrolistes@gmail.com">Association Union des Rôlistes & Co.</a> is licensed under <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/?ref=chooser-v1" target="_blank" rel="license noopener noreferrer" style="display:inline-block;">CC BY-NC-SA 4.0<img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1"><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1"><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/nc.svg?ref=chooser-v1"><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/sa.svg?ref=chooser-v1"></a></p> 
     </div>
 </footer>
