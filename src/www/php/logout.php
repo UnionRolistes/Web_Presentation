9ec3da7792789a17b58df32ec1837fad89992773
Youssef <agoussalyo@eisti.eu>
2021-08-11 18:41:57 +0200

    Fix logout script so it don't erase the webhook

--- src/www/php/logout.php
+++ src/www/php/logout.php
@@ -1,5 +1,9 @@
 <?php
 session_start();
-session_destroy(); //Ferme la session, detruit les vars associées
+//session_destroy(); //Ferme la session, detruit les vars associées --> Detruit aussi le Webhook ! 
+unset($_SESSION['user_id']);
+unset($_SESSION['pseudo']);
+unset($_SESSION['username']);
+unset($_SESSION['avatar_url']);
 header('Location:../index.php');
 ?>
